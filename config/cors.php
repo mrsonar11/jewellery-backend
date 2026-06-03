c61117a2eebd48a9f18b4efb0d9dbdf11c1d93f7
Manish Sonar <mrsonar11@gmail.com>
2026-06-03 11:19:28 +0530

    fix: force laravel to parse cloud Cors issue and clear config cache

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Vite default
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173')), // Vite default
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
